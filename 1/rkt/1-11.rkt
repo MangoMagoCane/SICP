8d7a6b7b6e82d70caba4c0826f1306696991a017
Val Garman <sir.mangocane@gmail.com>
2024-04-01 16:19:54 -0400

    feat: Add 1-11

--- 1/rkt/1-11.rkt
+++ 1/rkt/1-11.rkt
@@ -1,13 +1,23 @@
 #lang sicp
 
 ; recursive
-(define (f-r n)
+(define (fr n)
   (cond ((< n 3) n)
-        (else (+ (f-r (- n 1))
-                 (* 2 (f-r (- n 2)))
-                 (* 3 (f-r (- n 3)))))))
+        (else (+ (fr (- n 1))
+                 (* 2 (fr (- n 2)))
+                 (* 3 (fr (- n 3)))))))
 
-(f-r 3)
+(fr 7) ; 11
+; (+ (fr 3) (* 2 (fr 2)) (* 3 (fr 1)))
+; (+ (+ 2 2 0) (* 2 (fr 2)) (* 3 (fr 1)))
+; (+ 4 4 3)
+
 ; iterative
-(define (f-i n)
-  ())
+(define (fi n)
+  (define (fi-iter a b c count)
+    (if (< count 3)
+        a
+        (fi-iter (+ a (* 2 b) (* 3 c)) a b (- count 1))))
+  (fi-iter 2 1 0 n))
+
+(fi 7)
