e4f03193c19f1d6e6ff3337ee5ca924dfea33561
Val Garman <sir.mangocane@gmail.com>
2024-04-04 00:49:43 -0400

    fix: Complete 1-20

--- 1/rkt/1-20.rkt
+++ 1/rkt/1-20.rkt
@@ -7,21 +7,64 @@
       a
       (gcd b (remainder a b))))
 
-; NORMAL-ORDER
-; (gcd 206 40)
-; (gcd 40 (remainder 206 40))
-; 
-; 
-; 
-; 
 ; APPLICATIVE-ORDER: remainder called 4 times
-; (gcd 206 40)
-; (gcd 40 (remainder 206 40))
-; (gcd 40 6)
-; (gcd 6 (remainder 40 6))
-; (gcd 6 4)
-; (gcd 4 (remainder 6 4))
-; (gcd 4 2)
-; (gcd 2 (remainder 4 2))
-; (gcd 2 0)
-; 2
+(gcd 206 40)
+(gcd 40 (remainder 206 40))
+(gcd 40 6)
+(gcd 6 (remainder 40 6))
+(gcd 6 4)
+(gcd 4 (remainder 6 4))
+(gcd 4 2)
+(gcd 2 (remainder 4 2))
+(gcd 2 0)
+2
+
+; NORMAL-ORDER: remainder called 18 times
+(gcd 206 40)
+(if (= 40 0)
+    206
+    (gcd 40 (remainder 206 40)))
+(gcd 40 (remainder 206 40))
+(if (= (remainder 206 40) 0)
+    40
+    (gcd (remainder 206 40) 
+         (remainder 40 (remainder 206 40))))
+(if (= 6 0) ; + 1
+    40
+    (gcd (remainder 206 40) 
+         (remainder 40 (remainder 206 40))))
+(gcd (remainder 206 40) 
+         (remainder 40 (remainder 206 40)))
+(if (= (remainder 40 (remainder 206 40)) 0)
+    (remainder 206 40)
+    (gcd (remainder 40 (remainder 206 40)) 
+         (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))))
+(if (= 4 0) ; + 2
+    (remainder 206 40)
+    (gcd (remainder 40 (remainder 206 40)) 
+         (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))))
+(gcd (remainder 40 (remainder 206 40)) 
+         (remainder (remainder 206 40) (remainder 40 (remainder 206 40))))
+(if (= (remainder (remainder 206 40) (remainder 40 (remainder 206 40))) 0)
+    (remainder 40 (remainder 206 40))
+    (gcd (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))
+         (remainder (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40))))))
+(if (= 2 0) ; + 4
+    (remainder 40 (remainder 206 40))
+    (gcd (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))
+         (remainder (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40))))))
+(gcd (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))
+     (remainder (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))))
+(if (= (remainder (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))) 0)
+    (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))
+    (gcd (remainder (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40))))
+         (remainder (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))
+                    (remainder (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))))))
+(if (= 0 0) ; + 7
+    (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))
+    (gcd (remainder (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40))))
+         (remainder (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))
+                    (remainder (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))))))
+(remainder (remainder 206 40) (remainder 40 (remainder 206 40))) ; + 4
+
+
