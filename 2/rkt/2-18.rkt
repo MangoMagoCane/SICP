42f574cc55ff803a83545fa24df6d203628889f5
Val Garman <sir.mangocane@gmail.com>
2024-04-10 23:48:30 -0400

    feat: Add new reverse function

--- 2/rkt/2-18.rkt
+++ 2/rkt/2-18.rkt
@@ -7,11 +7,18 @@
       list2
       (cons (car list1) (append (cdr list1) list2))))
 
-(define (reverse ls)
+(define (reverse0 ls)
   (define (iter ls result)
     (if (null? ls)
         result
         (iter (cdr ls) (append (list (car ls)) result))))
   (iter ls nil))
+
+(define (reverse items)
+  (define (iter items result)
+    (if (null? items)
+        result
+        (iter (cdr items) (cons (car items) result))))
+  (iter items nil))
 
 (reverse (list 1 4 9 16 25))
