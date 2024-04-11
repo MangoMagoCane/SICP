369b293ccca12ef23a11a781c23c6470d3856fd4
Val Garman <sir.mangocane@gmail.com>
2024-04-10 23:49:21 -0400

    fix: complete 2-27

--- 2/rkt/2-27.rkt
+++ 2/rkt/2-27.rkt
@@ -1,12 +1,27 @@
 #lang sicp
 
+(define (reverse items)
+  (define (iter items result)
+    (if (null? items)
+        result
+        (iter (cdr items) (cons (car items) result))))
+  (iter items nil))
+
 (define (deep-reverse items)
-  ())
+  (if (not (pair? items)) 
+      items
+      (reverse (cons (deep-reverse (car items))
+                     (deep-reverse (cdr items))))))
+
+(define (deep-reverse2 items)
+  (if (not (pair? items)) 
+      items
+      (reverse (map deep-reverse2 items))))
 
 (define x (list (list 1 2) (list 3 4)))
-x
+; x
 ; ((1 2) (3 4))
-(reverse x)
+; (reverse x)
 ; ((3 4) (1 2))
-(deep-reverse x)
+; (deep-reverse2 x) 
 ; ((4 3) (2 1))
