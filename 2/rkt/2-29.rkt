20ad648538de2ea49386a7b1e561153ef0adb3a5
Val Garman <sir.mangocane@gmail.com>
2024-04-10 23:50:07 -0400

    feat: Add 2-28, Add 2-29

---
+++ 2/rkt/2-29.rkt
@@ -0,0 +1,22 @@
+#lang sicp
+
+(define (make-mobile left right)
+  (list left right))
+
+(define (make-branch length structure)
+  (list length structure))
+
+(define (left-branch mobile) 
+  (car mobile))
+(define (right-branch mobile)  
+  (car (cdr mobile)))
+
+(define (branch-length branch)
+  (car branch))
+(define (branch-structure branch)
+  (car (cdr branch)))
+
+
+
+(define x (make-mobile (make-branch 1 2) (make-branch 3 4)))
+(branch-structure (left-branch x))
