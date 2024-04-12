62487db0dfd9f0efbcbaba490b11bcb49d31fb64
Val Garman <sir.mangocane@gmail.com>
2024-04-12 09:49:53 -0400

    fix: Complete 2-33

--- 2/rkt/2-33.rkt
+++ 2/rkt/2-33.rkt
@@ -7,10 +7,12 @@
           (accumulate op initial (cdr sequence)))))
 
 (define (map p sequence)
-  (accumulate (lambda (x y) (x y)) nil sequence))
-; (define (append seq1 seq2)
-;   (accumulate cons ⟨??⟩ ⟨??⟩))
-; (define (length sequence)
-;   (accumulate ⟨??⟩ 0 sequence))
+  (accumulate (lambda (x y) (cons (p x) y)) nil sequence))
+(define (append seq1 seq2)
+  (accumulate cons seq2 seq1))
+(define (length sequence)
+  (accumulate (lambda (_x y) (+ y 1)) 0 sequence))
 
 (map (lambda (x) (* x x)) (list 1 2 3))
+(append (list 1 2) (list 3 4))
+(length (list 1 2 3 4 5 6 7))
