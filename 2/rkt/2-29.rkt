7d6f2b37ce68e8fcd6196049eae3dc7d72c3a1ed
Val Garman <sir.mangocane@gmail.com>
2024-04-12 09:50:17 -0400

    feat: Add 2-34 to 2-36

--- 2/rkt/2-29.rkt
+++ 2/rkt/2-29.rkt
@@ -2,15 +2,13 @@
 
 (define (make-mobile left right)
   (list left right))
-
-(define (make-branch length structure)
-  (list length structure))
-
 (define (left-branch mobile) 
   (car mobile))
 (define (right-branch mobile)  
   (car (cdr mobile)))
 
+(define (make-branch length structure)
+  (list length structure))
 (define (branch-length branch)
   (car branch))
 (define (branch-structure branch)
@@ -19,4 +17,6 @@
 
 
 (define x (make-mobile (make-branch 1 2) (make-branch 3 4)))
-(branch-structure (left-branch x))
+
+(define (total-weight mobile)
+  ())
---
+++ 2/rkt/2-35.rkt
@@ -0,0 +1,18 @@
+#lang sicp
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (count-leaves t)
+  (accumulate + 
+              0 
+              (map (lambda (t) 
+                      (cond ((null? t) 0)
+                            ((not (pair? t)) 1)
+                            (else (count-leaves t)))) 
+                   t)))
+
+(count-leaves (list 1 (list 2 3) (list 4 5 (list 6 7))))
