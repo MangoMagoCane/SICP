cc76b7e8637415b073da0d1e5292ab8354690acb
Val Garman <sir.mangocane@gmail.com>
2024-06-16 13:03:19 -0400

    fix: Update comment 2-34, Style 2-35

--- 2/rkt/2-35.rkt
+++ 2/rkt/2-35.rkt
@@ -7,12 +7,13 @@
           (accumulate op initial (cdr sequence)))))
 
 (define (count-leaves t)
-  (accumulate + 
-              0 
-              (map (lambda (t) 
-                      (cond ((null? t) 0)
-                            ((not (pair? t)) 1)
-                            (else (count-leaves t)))) 
-                   t)))
+  (accumulate 
+    + 
+    0 
+    (map (lambda (t) 
+            (cond ((null? t) 0)
+                  ((not (pair? t)) 1)
+                  (else (count-leaves t)))) 
+          t)))
 
 (count-leaves (list 1 (list 2 3) (list 4 5 (list 6 7))))
