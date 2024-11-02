0db8c5015c68c1ae1007fb0b800dfc743bfe59f2
Leonardo Garman <garmanmago@gmail.com>
2024-11-01 22:27:57 -0400

    feat: Add 3-32

---
+++ 3/rkt/3-32.rkt
@@ -0,0 +1,5 @@
+#lang sicp
+
+; it is important that the procedures for each segment are called
+; in the order they were added so that subsequent wires have the
+; correct signal propagated from previous wires
